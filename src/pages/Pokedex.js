ebefb9d2aa7c6debe63a23434e234eb0b16198fa
“Sam Manalo” <sam.manalo@supportcommunity.com>
2023-05-29 21:23:42 +0800

    Sort Filter and Stats

--- src/pages/Pokedex.js
+++ src/pages/Pokedex.js
@@ -15,12 +15,15 @@ import {
   TextField,
   InputAdornment,
   MenuItem,
-  Select
+  Select,
+  Button,
+  ToggleButton
 } from '@mui/material';
 import { grey } from '@mui/material/colors';
 
 import CloseIcon from '@mui/icons-material/Close';
 import SearchIcon from '@mui/icons-material/Search';
+import StraightIcon from '@mui/icons-material/Straight';
 
 import BugIcon from '../images/Pokemon_Type_Icon_Bug.svg';
 import DarkIcon from '../images/Pokemon_Type_Icon_Dark.svg';
@@ -50,49 +53,69 @@ function Test() {
   const { mode } = useStore();
   const [pokemonList, setPokemonList] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [sortOrder, setSortOrder] = useState('asc');
   const [searchText, setSearchText] = useState('');
   const [selectedType1, setSelectedType1] = useState('Any');
   const [selectedType2, setSelectedType2] = useState('Any');
   const [selectedGen, setSelectedGen] = useState('generation-i');
+  const [selectedStat, setSelectedStat] = useState('id'); 
 
   useEffect(() => {
     const fetchPokemonData = async () => {
       try {
-        const response = await axios.get('https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0');
+        const response = await axios.get('https://pokeapi.co/api/v2/pokemon?limit=151&offset=0');
         const data = response.data.results;
 
-        const formattedPokemonList = data.map(async (pokemon) => {
-          const res = await axios.get(pokemon.url);
-          const pokemonData = res.data;
+        const formattedPokemonList = await Promise.all(
+          data.map(async (pokemon) => {
+            const res = await axios.get(pokemon.url);
+            const pokemonData = res.data;
 
-          // Fetching generation information
-          const speciesResponse = await axios.get(pokemonData.species.url);
-          const generationUrl = speciesResponse.data.generation.url;
-          const generationResponse = await axios.get(generationUrl);
-          const generation = generationResponse.data.name;
+            // Fetching generation information
+            const speciesResponse = await axios.get(pokemonData.species.url);
+            const generationUrl = speciesResponse.data.generation.url;
+            const generationResponse = await axios.get(generationUrl);
+            const generation = generationResponse.data.name;
 
-          const formattedPokemonData = {
-            sprites: {
-              front_default: pokemonData.sprites.front_default,
-            },
-            id: pokemonData.id,
-            name: pokemonData.name,
-            types: pokemonData.types,
-            generation: generation, // Include the generation information
-          };
-          return formattedPokemonData;
+            const formattedPokemonData = {
+              sprites: {
+                front_default: pokemonData.sprites.front_default,
+              },
+              id: pokemonData.id,
+              name: pokemonData.name,
+              types: pokemonData.types,
+              generation: generation,
+              hp: pokemonData.stats.find((stat) => stat.stat.name === 'hp').base_stat,
+              atk: pokemonData.stats.find((stat) => stat.stat.name === 'attack').base_stat,
+              def: pokemonData.stats.find((stat) => stat.stat.name === 'defense').base_stat,
+              specialAttack: pokemonData.stats.find((stat) => stat.stat.name === 'special-attack').base_stat,
+              specialDefense: pokemonData.stats.find((stat) => stat.stat.name === 'special-defense').base_stat,
+              speed: pokemonData.stats.find((stat) => stat.stat.name === 'speed').base_stat,
+              total:
+                pokemonData.stats.reduce((total, stat) => total + stat.base_stat, 0),
+            };
+            return formattedPokemonData;
+          })
+        );
+
+        // Sort the list based on the selected stat
+        const sortedList = [...formattedPokemonList].sort((a, b) => {
+          if (sortOrder === 'asc') {
+            return a[selectedStat] - b[selectedStat]; // Ascending order
+          } else {
+            return b[selectedStat] - a[selectedStat]; // Descending order
+          }
         });
 
-        const resolvedPokemonList = await Promise.all(formattedPokemonList);
-        setPokemonList(resolvedPokemonList);
-        setIsLoading(false); // Set isLoading to false after data is fetched
+        setPokemonList(sortedList);
+        setIsLoading(false);
       } catch (error) {
         console.log(error);
       }
     };
 
     fetchPokemonData();
-  }, []);
+  }, [selectedStat]);
 
   const getTypeIcon = (type) => {
     const typeIcons = {
@@ -155,6 +178,31 @@ function Test() {
     { name: 'Gen IX', value: 'generation-ix', icon: `${spriteGen}909.png` },
   ];
 
+  const pokemonStat = [
+    { name: 'ID', value: 'id' },
+    { name: 'HP', value: 'hp' },
+    { name: 'ATK', value: 'atk' },
+    { name: 'DEF', value: 'def' },
+    { name: 'S.ATK', value: 'specialAttack' },
+    { name: 'S.DEF', value: 'specialDefense' },
+    { name: 'SPEED', value: 'speed' },
+    { name: 'TOTAL', value: 'total' }
+  ];
+
+  const sortPokemonList = () => {
+    const newSortOrder = sortOrder === 'asc' ? 'desc' : 'asc';
+    setSortOrder(newSortOrder); // Update sortOrder state
+
+    const sortedList = [...pokemonList].sort((a, b) => {
+      if (newSortOrder === 'asc') {
+        return a[selectedStat] - b[selectedStat]; // Ascending order
+      } else {
+        return b[selectedStat] - a[selectedStat]; // Descending order
+      }
+    });
+    setPokemonList(sortedList);
+  };
+
   const handleSearchTextChange = (event) => {
     setSearchText(event.target.value);
   };
@@ -173,6 +221,10 @@ function Test() {
 
   const clearSearchText = () => {
     setSearchText('');
+  };
+
+  const handleChange = (event) => {
+    setSelectedStat(event.target.value);
   };
 
   const truncatePokemonName = (name, maxLength) => {
@@ -215,8 +267,27 @@ function Test() {
       <Box sx={{ padding: '16px' }}>
         <Grid container sx={{ marginBottom: '16px' }} spacing={1}>
           <Grid item>
+            <ToggleButton
+              onClick={sortPokemonList}
+              value="check"
+              sx={{
+                height: '100%',
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center',
+              }}
+            >
+              <StraightIcon
+                sx={{
+                  transform: `rotate(${sortOrder === 'desc' ? '180deg' : '0deg'})`,
+                  transition: 'transform 0.3s ease',
+                }}
+              />
+            </ToggleButton>
+          </Grid>
+          <Grid item>
             <TextField
-              sx={{ maxWidth: { xs: '170px', sm: '170px', md: '170px', lg: '260px', xl: '260px' } }}
+              sx={{ maxWidth: { xs: '220px', sm: '290px', md: '290px', lg: '290px', xl: '290px' } }}
               id='outlined-basic'
               label='Search'
               variant='outlined'
@@ -337,7 +408,29 @@ function Test() {
               ))}
             </Select>
           </Grid>
-
+          <Grid item>
+            <Select
+              id='outlined-select-stat'
+              value={selectedStat}
+              onChange={(event) => setSelectedStat(event.target.value)}
+              label='Stat'
+              variant='outlined'
+              sx={{ minWidth: '125px' }}
+            >
+              {pokemonStat.map((type) => (
+                <MenuItem value={type.value} key={type.value}>
+                  <Stack direction='row' spacing={1} alignItems='center'>
+                    <CatchingPokemon
+                      sx={{ width: '20px', height: '20px', color: mode === 'dark' ? grey[200] : grey[600] }}
+                    />
+                    <Typography variant='body2' sx={{ fontSize: '14px' }}>
+                      {type.name}
+                    </Typography>
+                  </Stack>
+                </MenuItem>
+              ))}
+            </Select>
+          </Grid>
         </Grid>
         {isLoading ? (
           <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '50vh' }}>
@@ -364,8 +457,24 @@ function Test() {
                     <CardContent>
                       <Stack direction='row' spacing={1} alignItems='center'>
                         <Typography gutterBottom variant='body2' component='div' color='text.secondary'>
-                          #{pokemon.id.toString().padStart(3, '0')}
-                        </Typography>
+                        {selectedStat === 'id'
+                          ? `#${pokemon.id.toString().padStart(3, '0')}`
+                          : selectedStat === 'hp'
+                          ? `${pokemon.hp}HP`
+                          : selectedStat === 'atk'
+                          ? `${pokemon.atk}`
+                          : selectedStat === 'def'
+                          ? `${pokemon.def}`
+                          : selectedStat === 'specialAttack'
+                          ? `${pokemon.specialAttack}`
+                          : selectedStat === 'specialDefense'
+                          ? `${pokemon.specialDefense}`
+                          : selectedStat === 'speed'
+                          ? `${pokemon.speed}`
+                          : selectedStat === 'total'
+                          ? `${pokemon.total}`
+                          : `#${pokemon.id.toString().padStart(3, '0')}`}
+                      </Typography>
                       </Stack>
                       <Typography
                         sx={{ lineHeight: 1.2, maxHeight: '2.4em', overflow: 'hidden', textOverflow: 'ellipsis' }}
